f122510986918a81c7511404c7dd011265a9d93d
rzfd <rizkifadillah783@gmail.com>
2024-10-04 21:39:47 +0700

    Feat: add db conf

---
+++ internal/utill/db.go
@@ -0,0 +1,49 @@
+package utill
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/rzfd/gorm-ners/internal/config"
+	"github.com/rzfd/gorm-ners/internal/handlers/http/model"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+func ConnectDB() *gorm.DB {
+	dbHost := config.GetEnv("DB_HOST")
+	dbUser := config.GetEnv("DB_USER")
+	dbPassword := config.GetEnv("DB_PASSWORD")
+	dbName := config.GetEnv("DB_NAME")
+	dbPort := config.GetEnv("DB_PORT")
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Info,
+			IgnoreRecordNotFoundError: true,
+			ParameterizedQueries:      true,
+			Colorful:                  true,
+		},
+	)
+
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+		dbHost, dbUser, dbPassword, dbName, dbPort)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger:                 newLogger,
+		SkipDefaultTransaction: true,
+		PrepareStmt:            true,
+	})
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&model.User{}, &model.Company{}, &model.Regis{}); err != nil {
+		log.Fatalf("Failed to run migrations: %v", err)
+	}
+	return db
+}
